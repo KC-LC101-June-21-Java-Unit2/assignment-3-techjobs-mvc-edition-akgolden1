9abaec6e93f157a4e1ea93495936a361dbacd6d2
akgolden1 <akgolden1@users.noreply.github.com>
2021-11-11 20:17:04 -0600

    SearchController

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,16 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Array;
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -13,6 +20,7 @@ import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoic
  */
 @Controller
 @RequestMapping("search")
+
 public class SearchController {
 
     @GetMapping(value = "")
@@ -22,5 +30,20 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping ("results")//from form tag
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
 
+
+    //if the user enters all, or leave the search box empty, call findAll() from JobData. Need a variable to store that.
+        ArrayList<Job> jobs = new ArrayList<>();
+    if (searchTerm.toLowerCase().equals("all") || searchTerm.equals ("")){
+        jobs = JobData.findAll();
+    }
+    else {
+        jobs = JobData.findByColumnAndValue(searchType,searchTerm); //call findByColumnAndValue, pass it search terms from above.
+    }
+        model.addAttribute("jobs",jobs); //hopefully pass jobs to search.html using model
+        model.addAttribute("columns", columnChoices); //not sure about second param
+        return "search"; //returns the template. Params above taken from form and text
+    }
 }
